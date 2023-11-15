cc8ffa13444fc9957f27ea70ab128202a9523b8f
draobrehtom <draobrehtom@gmail.com>
2023-11-15 13:01:48 +0100

    Init

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version "cerulean"
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+file 'metapeds.ymt'
+data_file 'PED_METADATA_FILE' 'metapeds.ymt'
